8cf19b21aa4e93e54701f80ad62922e759b6b829
error529 <62388538+shanicegygenaar@users.noreply.github.com>
2021-02-04 11:18:00 +0200

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import  './About.css';
+import {Link} from 'react-router-dom';
+
+
+
+
+
+
+
+
+
+  function About() {
+    
+    return(
+      
+        <div className='pagetwo'>
+
+            <br />
+            <br />
+            <br />
+            
+            
+
+<h1>WE</h1>   
+            
+            
+            <div className='imagewrap'>
+            <div className='row'>
+              <div className='column'>
+            <img src={process.env.PUBLIC_URL + '/img/design.jpeg'} className='images'/><p>Design</p>
+            </div>
+            <div className='column'>
+            <img src={process.env.PUBLIC_URL + '/img/create.jpeg'} className='images'/><p>Create</p>
+            </div>
+            <div className='column'>
+            <img src={process.env.PUBLIC_URL + '/img/test.jpg'} className='images'/><p>Test</p>
+            </div>
+            <div className='column'>
+            <img src={process.env.PUBLIC_URL + '/img/launch.jpg'} className='images'/><p>Launch</p>
+            </div>
+            </div>
+
+            
+</div>
+
+
+<div className='about2' id='landingpage'>
+  <h1 className='about3'>About us</h1>
+  <div className='boxwrap'>
+ <p> <img src={process.env.PUBLIC_URL + '/img/about-image.jpg'} className='images2'/>
+ScrapeCoders is a result-driven, tech startup company founded in early 2020 by a group of six core developers. Each from different teach backgrounds that have a passions fro technology and who believed that technology is the driving force of change in communities and modern society.
+</p>
+
+
+
+  </div>
+
+</div>
+
+      
+          </div> 
+        
+    )
+}
+export default About;
+
